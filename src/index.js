48796693f6fd4a9f486da0d719c3b8725cec5f7f
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-16 20:30:18 +0300

    Add project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import store from './store/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // eslint-disable-next-line react/jsx-no-undef
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
